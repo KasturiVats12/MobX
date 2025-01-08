44f82cfa29a9da997c1d98dd22efc4b04a3e45a6
KasturiVats12 <highonnknowledge@gmail.com>
2025-01-08 23:58:50 +0530

    Update Input.js

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { useStore } from './Counter'
+// import React from 'react'
+// import { useStore } from './Counter'
 
-function Input() {
-  const {counterIncrement, counter} = useStore()
-  return (
-    <div>Counter:{counter}</div>
-  )
-}
+// function Input() {
+//   const {counterIncrement, counter} = useStore()
+//   return (
+//     <div>Counter:{counter}</div>
+//   )
+// }
 
-export default Input
+// export default Input
